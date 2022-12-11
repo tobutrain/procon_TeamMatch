78a1d6142b0cf92f221a9cecce72199eb1b7a140
tobutrain_10000 <lamy.yukihana@tobutrain.net>
2022-12-11 20:47:11 +0900

    csv reading test

--- TeamMatch.py
+++ TeamMatch.py
@@ -1,1 +1,16 @@
-print("Hello,World!")
+import csv
+
+def distWordToWord(word1,word2):
+    
+
+
+
+csv_header = ['ID','Word1','Word2','Word3']
+fileDirectory = "testFIle.csv"
+dictIDKeyword = {}
+num = 0
+with open(fileDirectory) as dictFile :
+    for row in csv.DictReader(dictFile,csv_header):
+        print(row)
+        dictIDKeyword[num] = row
+        num += 1
